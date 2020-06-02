b6bbe1fb0f93360dbeeb2d9d7a969d0643d82bfb
ChenYong <chenyong@jimeng.io>
2020-06-02 14:11:49 +0800

    handle cases other than PRs

--- index.js
+++ index.js
@@ -2,50 +2,55 @@ let core = require("@actions/core");
 let github = require("@actions/github");
 let cp = require("child_process");
 
-try {
+let detectChanged = () => {
   let pathname = core.getInput("path");
 
-  try {
-    // use changed=true by default
-    core.setOutput("changed", "true");
+  // use changed=true by default
+  core.setOutput("changed", "true");
 
-    let output = cp
-      .execSync(
-        // list changed file names relative to base commit
-        `git diff --name-only ${github.context.payload.pull_request.base.sha}`
-      )
-      .toString();
-    let changedPaths = output.trim().split("\n");
+  let output = cp
+    .execSync(
+      // list changed file names relative to base commit
+      `git diff --name-only ${github.context.payload.pull_request.base.sha}`
+    )
+    .toString();
+  let changedPaths = output.trim().split("\n");
 
-    if (changedPaths.length > 0) {
-      let targetPaths = changedPaths.filter((filepath) => {
-        return filepath.includes(pathname);
-      });
+  if (changedPaths.length > 0) {
+    let targetPaths = changedPaths.filter((filepath) => {
+      return filepath.includes(pathname);
+    });
 
-      console.log(
-        `Among ${changedPaths.length} files, targets are detected:`,
-        JSON.stringify(targetPaths, null, 2)
-      );
+    console.log(
+      `Among ${changedPaths.length} files, targets are detected:`,
+      JSON.stringify(targetPaths, null, 2)
+    );
 
-      if (targetPaths.length > 0) {
-        console.log("Has change in", pathname);
-        core.setOutput("changed", "true");
-      } else {
-        console.log("No change in", pathname);
-        core.setOutput("changed", "false");
-      }
+    if (targetPaths.length > 0) {
+      console.log("Has change in", pathname);
+      core.setOutput("changed", "true");
     } else {
-      console.log("Nothing changed in the PR");
+      console.log("No change in", pathname);
       core.setOutput("changed", "false");
     }
-  } catch (error) {
-    console.log("Failed", error);
+  } else {
+    console.log("Nothing changed in the PR");
+    core.setOutput("changed", "false");
   }
+};
 
+try {
   // Get the JSON webhook payload for the event that triggered the workflow
   let payload = JSON.stringify(github.context.payload);
   console.log(`The event payload:`);
-  console.log(payload)
+  console.log(payload);
+
+  if (payload.pull_request != null) {
+    detectChanged();
+  } else {
+    console.log("Always true after being merged");
+    core.setOutput("changed", "true");
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
